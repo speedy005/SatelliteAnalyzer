654c27dbf812fdc6735dea858b7199d07987e9ae
speedy005 <67871007+speedy005@users.noreply.github.com>
2025-08-25 14:23:20 +0200

    Update plugin.py

--- usr/lib/enigma2/python/Plugins/Extensions/CiefpSatelliteAnalyzer/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/CiefpSatelliteAnalyzer/plugin.py
@@ -1,7 +1,7 @@
 # plugin.py
 from Plugins.Plugin import PluginDescriptor
 from Screens.MessageBox import MessageBox
-from Plugins.Extensions.CiefpSatelliteAnalyzer.CiefpSatelliteAnalyzer import SatelliteAnalyzer
+from Plugins.Extensions.SatelliteAnalyzer.SatelliteAnalyzer import SatelliteAnalyzer
 
 
 PLUGIN_VERSION = "1.0"
@@ -11,7 +11,7 @@ def main(session, **kwargs):
 
 def Plugins(**kwargs):
     return PluginDescriptor(
-        name="CiefpSatelliteAnalyzer",
+        name="SatelliteAnalyzer",
         description=f"Show all information about the current channel (Version {PLUGIN_VERSION})",
         where=PluginDescriptor.WHERE_PLUGINMENU,
         icon="satellite.png",
